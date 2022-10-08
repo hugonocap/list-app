c552dca980ac46a79b933da2eb9c3c188e94cb7a
Hugo Montana <benzlbenny2k@gmail.com>
2022-10-09 00:37:05 +0300

    Initial commit

---
+++ customListApp/customListApp/customListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  customListAppApp.swift
+//  customListApp
+//
+//  Created by Hugo on 9.10.22.
+//
+
+import SwiftUI
+
+@main
+struct customListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
